f326de3fabc64a653e1b40b55bf7fd7eec4f13e0
PYRO_PRIEST!! <85893956+PYROPRIEST@users.noreply.github.com>
2023-04-25 12:15:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,6 +7,8 @@ def is_enabled(value, default):
         return True
     elif value.lower() in ["false", "no", "0", "disable", "n"]:
         return False
+elif value.lower() in ["None"]:
+        return NoneType
     else:
         return default
 
